e79a2c03771f9ae60da68f16bc805068343f2a5e
Edward Zhang <encryptededdy@users.noreply.github.com>
2017-04-30 16:08:42 +1200

    Update speedtest.php

--- speedtest.php
+++ speedtest.php
@@ -1,5 +1,6 @@
 <?php
-$darkskyapi = "<removed>"; // needed for tracking precipitation
+$darkskyapi = "63806295f8572e6db8decede5e0549bd"; // needed for tracking precipitation
+
 try {
     $json = file_get_contents("https://api.darksky.net/forecast/".$darkskyapi."/-36.8536,174.776?units=si");
     $response = json_decode($json, true);
@@ -7,6 +8,6 @@ try {
 } catch (Exception $exp) {
     $percipIntensity = "ERR";
 }
-$speedtest = trim(shell_exec("/usr/local/bin/speedtest-cli --csv --server 5749"));
+$speedtest = trim(shell_exec("/usr/local/bin/speedtest-cli --csv --server 5749")); // recommended that you use a fixed server instead of autodetect.
 printf("%s,%s\n", $speedtest, $percipIntensity);
 ?>
